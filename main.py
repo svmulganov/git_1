9bca601ac3d3e444371890ba0956d00d6a2ce600
mulganov <mulganovsv@yandexlyceum.ru>
2023-11-23 18:44:08 +0300

    comment 4

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm project')
+    print_hi('PyCharm project create')
+    print('ok')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
